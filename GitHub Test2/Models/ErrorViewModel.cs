1e43e0187ad4bea1b079be6a4d48691fa665b19e
acer-swift3 <acer-swift3@LAPTOP-7MQ3G9N7>
2022-10-09 18:25:10 +0800

    加入專案檔案。

---
+++ GitHub Test2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHub_Test2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
